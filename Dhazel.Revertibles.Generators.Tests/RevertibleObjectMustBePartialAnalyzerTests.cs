111c195a4a9a0617e362033ba93d9514c4487d3c
Timothy HALL <Timothy.HALL@odf.oregon.gov>
2026-08-10 14:38:08 -0700

    feat: add analyzer to make sure host class is partial

---
+++ Dhazel.Revertibles.Generators.Tests/RevertibleObjectMustBePartialAnalyzerTests.cs
@@ -0,0 +1,169 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Testing;
+using Microsoft.CodeAnalysis.Testing;
+
+namespace Dhazel.Revertibles.Generators.Tests;
+
+public class RevertibleObjectMustBePartialAnalyzerTests
+{
+    [Fact]
+    public async Task Error_WhenClassIsNotPartial()
+    {
+        const string source = """
+            using Dhazel.Revertibles;
+
+            namespace MyApp;
+
+            [RevertibleObject]
+            public class {|#0:Widget|}
+            {
+                [Revertible]
+                public string Name { get; set; } = "";
+            }
+            """;
+
+        var test = new CSharpAnalyzerTest<RevertibleObjectMustBePartialAnalyzer, DefaultVerifier>
+        {
+            ReferenceAssemblies = ReferenceAssemblies.Net.Net100,
+            TestState =
+            {
+                Sources = { source },
+                AdditionalReferences = { typeof(RevertibleObjectAttribute).Assembly },
+            },
+            ExpectedDiagnostics =
+            {
+                new DiagnosticResult(RevertibleObjectMustBePartialAnalyzer.Rule)
+                    .WithLocation(0)
+                    .WithArguments("Widget"),
+            },
+        };
+
+        await test.RunAsync();
+    }
+
+    [Fact]
+    public async Task NoDiagnostic_WhenClassIsPartial()
+    {
+        const string source = """
+            using Dhazel.Revertibles;
+
+            namespace MyApp;
+
+            [RevertibleObject]
+            public partial class Widget
+            {
+                [Revertible]
+                public string Name { get; set; } = "";
+            }
+            """;
+
+        var test = new CSharpAnalyzerTest<RevertibleObjectMustBePartialAnalyzer, DefaultVerifier>
+        {
+            ReferenceAssemblies = ReferenceAssemblies.Net.Net100,
+            TestState =
+            {
+                Sources = { source },
+                AdditionalReferences = { typeof(RevertibleObjectAttribute).Assembly },
+            }
+        };
+
+        await test.RunAsync();
+    }
+
+    [Fact]
+    public async Task NoDiagnostic_WhenClassHasNoRevertibleObjectAttribute()
+    {
+        const string source = """
+            namespace MyApp;
+
+            public class PlainWidget
+            {
+                public string Name { get; set; } = "";
+            }
+            """;
+
+        var test = new CSharpAnalyzerTest<RevertibleObjectMustBePartialAnalyzer, DefaultVerifier>
+        {
+            ReferenceAssemblies = ReferenceAssemblies.Net.Net100,
+            TestState =
+            {
+                Sources = { source },
+                AdditionalReferences = { typeof(RevertibleObjectAttribute).Assembly },
+            }
+        };
+
+        await test.RunAsync();
+    }
+
+    [Fact]
+    public async Task NoDiagnostic_WhenNonPartialClassHasUnrelatedAttribute()
+    {
+        // Sanity check: a random other attribute shouldn't trigger REV001.
+        const string source = """
+            using System;
+
+            namespace MyApp;
+
+            public class ObsoleteAttribute2 : Attribute { }
+
+            [ObsoleteAttribute2]
+            public class Widget
+            {
+                public string Name { get; set; } = "";
+            }
+            """;
+
+        var test = new CSharpAnalyzerTest<RevertibleObjectMustBePartialAnalyzer, DefaultVerifier>
+        {
+            ReferenceAssemblies = ReferenceAssemblies.Net.Net100,
+            TestState =
+            {
+                Sources = { source },
+                AdditionalReferences = { typeof(RevertibleObjectAttribute).Assembly },
+            }
+        };
+
+        await test.RunAsync();
+    }
+
+    [Fact]
+    public async Task Error_FiresOncePerNonPartialClass_NotOncePerAttributeUsage()
+    {
+        // Two separate non-partial classes, each decorated -> two diagnostics, not one.
+        const string source = """
+            using Dhazel.Revertibles;
+
+            namespace MyApp;
+
+            [RevertibleObject]
+            public class {|#0:WidgetA|}
+            {
+                [Revertible]
+                public string Name { get; set; } = "";
+            }
+
+            [RevertibleObject]
+            public class {|#1:WidgetB|}
+            {
+                [Revertible]
+                public string Title { get; set; } = "";
+            }
+            """;
+
+        var test = new CSharpAnalyzerTest<RevertibleObjectMustBePartialAnalyzer, DefaultVerifier>
+        {
+            ReferenceAssemblies = ReferenceAssemblies.Net.Net100,
+            TestState =
+            {
+                Sources = { source },
+                AdditionalReferences = { typeof(RevertibleObjectAttribute).Assembly },
+            },
+            ExpectedDiagnostics =
+            {
+                new DiagnosticResult(RevertibleObjectMustBePartialAnalyzer.Rule).WithLocation(0).WithArguments("WidgetA"),
+                new DiagnosticResult(RevertibleObjectMustBePartialAnalyzer.Rule).WithLocation(1).WithArguments("WidgetB"),
+            },
+        };
+
+        await test.RunAsync();
+    }
+}
---
+++ Dhazel.Revertibles.Generators/RevertibleObjectMustBePartialAnalyzer.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+
+namespace Dhazel.Revertibles.Generators;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class RevertibleObjectMustBePartialAnalyzer : DiagnosticAnalyzer
+{
+    public static readonly DiagnosticDescriptor Rule = new(
+        id: "REV001",
+        title: "RevertibleObject class must be partial",
+        messageFormat: "Class '{0}' is marked with [RevertibleObject] but is not declared 'partial'",
+        category: "Dhazel.Revertibles",
+        defaultSeverity: DiagnosticSeverity.Error,
+        isEnabledByDefault: true);
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => [Rule];
+
+    public override void Initialize(AnalysisContext context)
+    {
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+        context.EnableConcurrentExecution();
+        context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+    }
+
+    private static void AnalyzeSymbol(SymbolAnalysisContext context)
+    {
+        var symbol = (INamedTypeSymbol)context.Symbol;
+
+        var hasAttribute = symbol.GetAttributes()
+            .Any(a => a.AttributeClass?.ToDisplayString() == "Dhazel.Revertibles.RevertibleObjectAttribute");
+
+        if (!hasAttribute) return;
+
+        var isPartial = symbol.DeclaringSyntaxReferences
+            .Select(r => r.GetSyntax())
+            .OfType<ClassDeclarationSyntax>()
+            .Any(c => c.Modifiers.Any(SyntaxKind.PartialKeyword));
+
+        if (!isPartial)
+        {
+            context.ReportDiagnostic(Diagnostic.Create(Rule, symbol.Locations[0], symbol.Name));
+        }
+    }
+}
