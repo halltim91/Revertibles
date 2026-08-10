46e64c278d05df31d3543a2ff22c23c2aca11d1a
Timothy HALL <Timothy.HALL@odf.oregon.gov>
2026-08-10 11:05:17 -0700

    feat: add revertible object attribute

---
+++ Dhazel.Revertibles/RevertibleObjectAttribute.cs
@@ -0,0 +1,8 @@
+﻿namespace Dhazel.Revertibles;
+
+[AttributeUsage(AttributeTargets.Class)]
+public sealed class RevertibleObjectAttribute : Attribute
+{
+    /// <summary> When false, pristine values are NOT captured automatically. Manually calling AcceptChanges() will be required to begin change tracking </summary>
+    public bool AcceptPristineValues { get; set; } = true;
+}
